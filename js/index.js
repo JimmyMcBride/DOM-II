6504a911951719e31e36d79fe740b3167620cd6a
JimmyMcBride <mcbride967@gmail.com>
2019-04-12 11:34:03 -0500

    added hover color event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-// Your code goes here
+// My code is here:
+
+const navItems = document.querySelectorAll('a')
+
+// button hover color function
+
+navItems.forEach((value) => {
+    value.addEventListener('mouseover', function(event){
+        event.target.style.color = 'green'
+    })
+})
+
+navItems.forEach((value) => {
+    value.addEventListener('mouseout', function(event){
+        event.target.style.color = 'black'
+    })
+})
