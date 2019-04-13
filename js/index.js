2ad4a4b450d0b122dc91d972395d617b30dc340c
JimmyMcBride <mcbride967@gmail.com>
2019-04-13 10:02:57 -0500

    added more animation events to images and buttons

--- js/index.js
+++ js/index.js
@@ -1,25 +1,10 @@
 // My code is here:
 
-const navItems = document.querySelectorAll('a')
-const headerImage = document.querySelector('img')
+// Animations
 
-// Navbar hover color function
+    // Shake animations
 
-navItems.forEach((value) => {
-    value.addEventListener('mouseover', function(event){
-        event.target.style.color = 'orange'
-    })
-})
-
-navItems.forEach((value) => {
-    value.addEventListener('mouseout', function(event){
-        event.target.style.color = 'black'
-    })
-})
-
-// Navbar hover animation functions
-
-const shakeAnimation = function(){
+const slowShake = function(){
 
     this.animate([
         {transform: 'translate(1px, 1px) rotate(0deg)'},
@@ -40,7 +25,7 @@ const shakeAnimation = function(){
     })
 }
 
-const leaveShake = function(){
+const fastShake = function(){
     this.animate([
         {transform: 'translate(1px, 1px) rotate(0deg)'},
         {transform: 'translate(-1px, -2px) rotate(-1deg)'},
@@ -55,17 +40,9 @@ const leaveShake = function(){
     })
 }
 
-navItems.forEach((value) => {
-    value.addEventListener('mouseenter', shakeAnimation)
-})
+    // Spin animation
 
-navItems.forEach((value) => {
-    value.addEventListener('mouseleave', leaveShake)
-})
-
-// Header image load animation
-
-const spinAnimation = function(){
+const batmanAnimation = function(){
     this.animate([
         {transform: 'scale(.25)', opacity: .5},
         {transform: 'scale(.5) rotate(900deg)', opacity: .75},
@@ -73,10 +50,74 @@ const spinAnimation = function(){
         ], {
         duration: 2000,
         easing: 'ease-in-out',
-        delay: 10,
         iterations: 1,
         direction: 'normal',
         })
 }
 
-headerImage.addEventListener('load', spinAnimation)
+const spinToWin = function(){
+    this.animate([
+        {transform: 'rotateY(180deg)'},
+        {transform: 'rotateX(180deg)'},
+        {transform: 'rotateY(180deg)'},
+        {transform: 'rotateX(180deg)'},
+        // {transform: 'rotateY(90deg)'},
+        // {transform: 'rotateX(90deg)'},
+        ], {
+        duration: 250,
+        easing: 'ease-in-out',
+        iterations: 1,
+        direction: 'alternate',
+        })
+}
+
+// Navbar hover events
+
+const navItems = document.querySelectorAll('a')
+
+navItems.forEach((value) => {
+    value.addEventListener('mouseover', function(event){
+        event.target.style.color = 'orange'
+    })
+})
+
+navItems.forEach((value) => {
+    value.addEventListener('mouseout', function(event){
+        event.target.style.color = 'black'
+    })
+})
+
+navItems.forEach((value) => {
+    value.addEventListener('mouseover', slowShake)
+})
+
+navItems.forEach((value) => {
+    value.addEventListener('mouseout', fastShake)
+})
+
+navItems.forEach((value) => {
+    value.addEventListener('click', fastShake)
+})
+
+// Header image load event
+
+const headerImage = document.querySelector('img')
+const images = document.querySelectorAll('img')
+
+headerImage.addEventListener('load', batmanAnimation)
+
+// Image flip event
+
+images.forEach((value) => {
+    value.addEventListener('click', spinToWin)
+})
+
+// Button event
+
+const btn = document.querySelectorAll('.btn')
+
+btn.forEach((value) => {
+    value.addEventListener('click', slowShake)
+})
+
+// Sign up section
